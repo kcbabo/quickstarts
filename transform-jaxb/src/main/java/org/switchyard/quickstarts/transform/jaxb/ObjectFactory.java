41176d4069e98a97acf05368b6c3aedd36aa22cf
Keith Babo <kbabo@redhat.com>
2011-07-18 15:07:53 -0400

    Tweaked wsdl and tests

--- transform-jaxb/src/main/java/org/switchyard/quickstarts/transform/jaxb/ObjectFactory.java
+++ transform-jaxb/src/main/java/org/switchyard/quickstarts/transform/jaxb/ObjectFactory.java
@@ -1,3 +1,21 @@
+/* 
+ * JBoss, Home of Professional Open Source 
+ * Copyright 2010-2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved. 
+ * See the copyright.txt in the distribution for a 
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use, 
+ * modify, copy, or redistribute it subject to the terms and conditions 
+ * of the GNU Lesser General Public License, v. 2.1. 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+ * details. You should have received a copy of the GNU Lesser General Public 
+ * License, v.2.1 along with this distribution; if not, write to the Free 
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
+ * 02110-1301, USA.
+ */
 
 package org.switchyard.quickstarts.transform.jaxb;
 
@@ -10,7 +28,7 @@ import javax.xml.namespace.QName;
 /**
  * This object contains factory methods for each 
  * Java content interface and Java element interface 
- * generated in the org.switchyard.quickstarts.transform.jaxb package. 
+ * generated in the quickstart.switchyard.transform_jaxb._1 package. 
  * <p>An ObjectFactory allows you to programatically 
  * construct new instances of the Java representation 
  * for XML content. The Java representation of XML 
@@ -24,32 +42,14 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _SubmitOrder_QNAME = new QName("urn:switchyard-quickstart-transform-jaxb:1.0", "submitOrder");
-    private final static QName _SubmitOrderResponse_QNAME = new QName("urn:switchyard-quickstart-transform-jaxb:1.0", "submitOrderResponse");
-    private final static QName _OrderAck_QNAME = new QName("urn:switchyard-quickstart-transform-jaxb:1.0", "orderAck");
-    private final static QName _Order_QNAME = new QName("urn:switchyard-quickstart-transform-jaxb:1.0", "order");
+    private final static QName _OrderAck_QNAME = new QName("urn:switchyard-quickstart:transform-jaxb:1.0", "orderAck");
+    private final static QName _Order_QNAME = new QName("urn:switchyard-quickstart:transform-jaxb:1.0", "order");
 
     /**
-     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.switchyard.quickstarts.transform.jaxb
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: quickstart.switchyard.transform_jaxb._1
      * 
      */
     public ObjectFactory() {
-    }
-
-    /**
-     * Create an instance of {@link Order }
-     * 
-     */
-    public Order createOrder() {
-        return new Order();
-    }
-
-    /**
-     * Create an instance of {@link OrderAckType }
-     * 
-     */
-    public OrderAckType createOrderAckType() {
-        return new OrderAckType();
     }
 
     /**
@@ -61,47 +61,29 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link OrderType }
+     * Create an instance of {@link Order }
      * 
      */
-    public OrderType createOrderType() {
-        return new OrderType();
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link Order }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "urn:switchyard-quickstart-transform-jaxb:1.0", name = "submitOrder")
-    public JAXBElement<Order> createSubmitOrder(Order value) {
-        return new JAXBElement<Order>(_SubmitOrder_QNAME, Order.class, null, value);
+    public Order createOrder() {
+        return new Order();
     }
 
     /**
      * Create an instance of {@link JAXBElement }{@code <}{@link OrderAck }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "urn:switchyard-quickstart-transform-jaxb:1.0", name = "submitOrderResponse")
-    public JAXBElement<OrderAck> createSubmitOrderResponse(OrderAck value) {
-        return new JAXBElement<OrderAck>(_SubmitOrderResponse_QNAME, OrderAck.class, null, value);
+    @XmlElementDecl(namespace = "urn:switchyard-quickstart:transform-jaxb:1.0", name = "orderAck")
+    public JAXBElement<OrderAck> createOrderAck(OrderAck value) {
+        return new JAXBElement<OrderAck>(_OrderAck_QNAME, OrderAck.class, null, value);
     }
 
     /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link OrderAckType }{@code >}}
+     * Create an instance of {@link JAXBElement }{@code <}{@link Order }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "urn:switchyard-quickstart-transform-jaxb:1.0", name = "orderAck")
-    public JAXBElement<OrderAckType> createOrderAck(OrderAckType value) {
-        return new JAXBElement<OrderAckType>(_OrderAck_QNAME, OrderAckType.class, null, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link OrderType }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "urn:switchyard-quickstart-transform-jaxb:1.0", name = "order")
-    public JAXBElement<OrderType> createOrder(OrderType value) {
-        return new JAXBElement<OrderType>(_Order_QNAME, OrderType.class, null, value);
+    @XmlElementDecl(namespace = "urn:switchyard-quickstart:transform-jaxb:1.0", name = "order")
+    public JAXBElement<Order> createOrder(Order value) {
+        return new JAXBElement<Order>(_Order_QNAME, Order.class, null, value);
     }
 
 }
